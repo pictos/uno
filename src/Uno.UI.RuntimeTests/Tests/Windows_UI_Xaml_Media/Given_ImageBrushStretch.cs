818049831d6829ae089fa7369a48b5fbb6ff527b
Amélie Lachapelle-Dagenais <amelie.lachapelle@nventive.com>
2022-09-21 10:30:57 -0400

    refactor: Moving ImageBrush_Stretch to Runtime

--- src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Media/Given_ImageBrushStretch.cs
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Media/Given_ImageBrushStretch.cs
@@ -1,22 +1,16 @@
 ﻿using System;
-using System.Drawing;
-using System.Linq;
-using NUnit.Framework;
-using Uno.UI.RuntimeTests.Helpers;
 using System.Threading.Tasks;
-using Uno.UI.RuntimeTests.Extensions;
-using Windows.UI.Xaml.Media;
-using static Private.Infrastructure.TestServices;
-using Windows.UI.Xaml.Media.Imaging;
-using Color = System.Drawing.Color;
+using FluentAssertions.Formatting;
 using Private.Infrastructure;
-using Windows.UI.Xaml.Controls;
+using Uno.UI.RuntimeTests.Helpers;
 using Windows.UI.Xaml;
-using Windows.Foundation;
-using Windows.UI.Xaml.Shapes;
-using Rectangle = Windows.UI.Xaml.Shapes.Rectangle;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using static Private.Infrastructure.TestServices;
 using ImageBrush = Windows.UI.Xaml.Media.ImageBrush;
-using static Windows.UI.Xaml.Controls.CollectionChangedOperation;
+using Grid = Windows.UI.Xaml.Controls.Grid;
+using Rectangle = Windows.UI.Xaml.Shapes.Rectangle;
 
 namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Media
 {
@@ -29,108 +23,84 @@ namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Media
 		private const string Greenish = "#FF0ED145";
 		private const string White = "#FFFFFFFF";
 
-		//#if __SKIA__
+		private const double Width = 50;
+		private const double Height = 50;
+		private const double CenterX = 25;
+		private const double CenterY = 25;
+
+
+
+#if __SKIA__
 		[TestMethod]
 		[RunsOnUIThread]
 
 		public async Task When_Stretch()
 		{
 
-			var brush = new ImageBrush();
+			ImageBrush brush = new ImageBrush();
+
+			var SUT = new Grid()
+			{
+				Height = Height,
+				Width = Width,
+			};
+
+			SUT.Background = brush;
 			brush.Stretch = Stretch.Fill;
-
-			var rect = new Rectangle()
-			{
-				Fill = brush,
-				Height = 50,
-				Width = 50,
-				HorizontalAlignment = HorizontalAlignment.Left,
-				VerticalAlignment = VerticalAlignment.Top,
-			};
-
-			var border = new Border()
-			{
-				Child = rect,
-			};
-
-			TestServices.WindowHelper.WindowContent = border;
-
-			await TestServices.WindowHelper.WaitForIdle();
-
-			await TestServices.WindowHelper.WaitForIdle();
-
-			WindowHelper.WaitForLoaded(border);
-
+			
+			TestServices.WindowHelper.WindowContent = SUT;			
+			WindowHelper.WaitForLoaded(SUT);
 
 			var renderer = new RenderTargetBitmap();
-
-			await renderer.RenderAsync(border);
-
-			var fill = new RawBitmap(renderer, border);
-
-			Assert.IsNotNull(fill);
-
-			double centerX = fill.Width / 2;
-			double centerY = fill.Height / 2;
+			var fill = new RawBitmap(renderer, SUT);
+			await renderer.RenderAsync(SUT);
+			
+			await TestServices.WindowHelper.WaitForIdle();
+			await TestServices.WindowHelper.WaitForIdle();
 
 			// All edges are red-ish
-			await ImageAssert.HasColorAtChild(fill, border, centerX, fill.Height - 6, Redish, tolerance: 5);
-			await ImageAssert.HasColorAtChild(fill,border, centerX, 6, Redish, tolerance: 5);
-			await ImageAssert.HasColorAtChild(fill, border,  6, centerY, Redish, tolerance: 5);
-			await ImageAssert.HasColorAtChild(fill, border, fill.Width - 6, centerY, Redish, tolerance: 5);
+			await ImageAssert.HasColorAtChild(fill, SUT, CenterX, Height - 1, Yellowish, tolerance: 5);
+			await ImageAssert.HasColorAtChild(fill, SUT, CenterX, 1, Redish, tolerance: 5);
+			await ImageAssert.HasColorAtChild(fill, SUT,  1, CenterY, Redish, tolerance: 5);
+			await ImageAssert.HasColorAtChild(fill, SUT, Width - 1, CenterY, Redish, tolerance: 5);
 
-			brush.Stretch = Stretch.Uniform;
+			brush.Stretch = Stretch.UniformToFill;
 			await WindowHelper.WaitForIdle();
-			await renderer.RenderAsync(border);
-			var uniFill = new RawBitmap(renderer, border);
+			await renderer.RenderAsync(SUT);
+			var uniFill = new RawBitmap(renderer, SUT);
 			await WindowHelper.WaitForIdle();
-			centerX = uniFill.Width / 2;
-			centerY = uniFill.Height / 2;
 
 			// Top and bottom are red-ish. Left and right are yellow-ish
-			await ImageAssert.HasColorAt(uniformToFill, width, height + 6, Redish, tolerance: 5);
-			//	ImageAssert.HasColorAt(uniformToFill, centerX, 0 + 6, Redish, tolerance: 5);
-			//	ImageAssert.HasColorAt(uniformToFill, 6, centerY, Yellowish, tolerance: 5);
-			//	ImageAssert.HasColorAt(uniformToFill, width - 6, centerY, Yellowish, tolerance: 5);
-			//	
+			await ImageAssert.HasColorAtChild(uniFill, SUT, CenterX, Height - 1, Redish, tolerance: 5);
+			await ImageAssert.HasColorAtChild(uniFill, SUT, CenterX, 1, Redish, tolerance: 5);
+			await ImageAssert.HasColorAtChild(uniFill, SUT, 1, CenterY, Yellowish, tolerance: 5);
+			await ImageAssert.HasColorAtChild(uniFill, SUT, Width - 1, CenterY, Yellowish, tolerance: 5);
+
+			brush.Stretch = Stretch.UniformToFill;
+			await WindowHelper.WaitForIdle();
+			await renderer.RenderAsync(SUT);
+			var uniform = new RawBitmap(renderer, SUT);
+			await WindowHelper.WaitForIdle();
+
+			// Top and bottom are same as backround. Left and right are red-ish
+			await ImageAssert.HasColorAtChild(uniform, SUT, CenterX, Height + 1, White, tolerance: 5);
+			await ImageAssert.HasColorAtChild(uniform, SUT, CenterX, 1, White, tolerance: 5);//Test devrait plante true white
+		    await ImageAssert.HasColorAtChild(uniform, SUT, 1, CenterY, Redish, tolerance: 5);
+			await ImageAssert.HasColorAtChild(uniform,SUT, Width - 1, CenterY, Redish, tolerance: 5);
+
+			brush.Stretch = Stretch.None;
+			await WindowHelper.WaitForIdle();
+			await renderer.RenderAsync(SUT);
+			var none = new RawBitmap(renderer, SUT);
+			await WindowHelper.WaitForIdle();
+
+			// Everything is green-ish
+			await ImageAssert.HasColorAtChild(none, SUT, CenterX, Height - 1, Greenish, tolerance: 5);
+			await ImageAssert.HasColorAtChild(none, SUT, CenterX, 1, Greenish, tolerance: 5);
+			await ImageAssert.HasColorAtChild(none, SUT, 1, CenterY, Greenish, tolerance: 5);
+			await ImageAssert.HasColorAtChild(none, SUT, Width - 1, CenterY, Greenish, tolerance: 5);
 		}
-
-		//	[TestMethod]
-		////	public async Task When_Stretch_Uniform()
-		//	{
-
-		//		var brush = new ImageBrush()
-		//		{
-		//			Stretch = Stretch.Uniform,
-		//		};
-		//		WindowHelper.WindowContent = brush;
-		//		await WindowHelper.WaitForIdle();
-		//		await WindowHelper.WaitForIdle();
-		//		var renderer = new RenderTargetBitmap();
-		//		await WindowHelper.WaitForIdle();
-		//		await renderer.RenderAsync(brush);
-		//		var uniform = new RawBitmap(renderer, brush);
-		//		await WindowHelper.WaitForIdle();
-		//		float width = uniform.Width;
-		//		float height = uniform.Height;
-		//		float centerX = width / 2;
-		//		float centerY = height / 2;
-
-		// Top and bottom are same as backround. Left and right are red-ish
-		//		ImageAssert.HasColorAt(uniform, centerX, height + 6, White, tolerance: 5);
-		//		ImageAssert.HasColorAt(uniform, centerX, 6, White, tolerance: 5);
-		//		ImageAssert.HasColorAt(uniform, 6, centerY, Redish, tolerance: 5);
-		//		ImageAssert.HasColorAt(uniform, width - 6, centerY, Redish, tolerance: 5);
-		//		Assert.IsTrue(true);
-		//	}
-
-		// Everything is green-ish
-		//ImageAssert.HasColorAt(screenshot, none.CenterX, none.Y + 6, Greenish, tolerance: 5);
-		//ImageAssert.HasColorAt(screenshot, none.CenterX, none.Bottom - 6, Greenish, tolerance: 5);
-		//ImageAssert.HasColorAt(screenshot, none.X + 6, none.CenterY, Greenish, tolerance: 5);
-		//ImageAssert.HasColorAt(screenshot, none.Right - 6, none.CenterY, Greenish, tolerance: 5);
-		//}
-		//#endif
+#endif
 	}
 }
 
